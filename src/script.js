452ad442e2c68938b8db406a3729ac49dcc55b04
mailism <domiam09@gmail.com>
2024-08-06 17:20:47 +0700

    add-category

--- src/script.js
+++ src/script.js
@@ -16,9 +16,22 @@ document.querySelector('#close-search').onclick = () =>{
     searchForm.classList.remove('active');
 }
 
-
 window.onscroll = () =>{
     navbar.classList.remove('active');
+
+    if(window.scrollY > 0){
+        document.querySelector('.header').classList.add('active');
+    }else{
+        document.querySelector('.header').classList.remove('active');
+    }
+};
+
+window.onload = () =>{
+    if(window.scrollY > 0){
+        document.querySelector('.header').classList.add('active');
+    }else{
+        document.querySelector('.header').classList.remove('active');
+    }
 };
 
 var swiper = new Swiper(".home-slider", {
