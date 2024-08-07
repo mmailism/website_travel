30822767d87b908e66bd8857eb3d42d587c9b02b
mailism <domiam09@gmail.com>
2024-08-07 17:10:41 +0700

    add-product-a

--- src/script.js
+++ src/script.js
@@ -42,3 +42,27 @@ var swiper = new Swiper(".home-slider", {
     prevEl: ".swiper-button-prev",
   },
 });
+
+var swiper = new Swiper(".product-slider", {
+    loop:true,
+    grabCursor:true,
+    spaceBetween:20,
+    navigation: {
+    nextEl: ".swiper-button-next",
+    prevEl: ".swiper-button-prev",
+  },
+  breakpoints: {
+    0: {
+      slidesPerView: 1 ,
+    },
+    400: {
+      slidesPerView: 2,
+    },
+    768: {
+      slidesPerView: 3,
+    },
+    1020: {
+      slidesPerView: 4,
+    },
+  },
+});
