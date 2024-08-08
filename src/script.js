1545a70a4595626fbb9a8f574bac58862e6f3f33
mailism <domiam09@gmail.com>
2024-08-08 17:46:31 +0700

    add-footer

--- src/script.js
+++ src/script.js
@@ -108,3 +108,27 @@ breakpoints: {
   },
 },
 });
+
+var swiper = new Swiper(".clients-slider", {
+  loop:true,
+  grabCursor:true,
+  spaceBetween:20,
+  navigation: {
+  nextEl: ".swiper-button-next",
+  prevEl: ".swiper-button-prev",
+},
+breakpoints: {
+  0: {
+    slidesPerView: 1 ,
+  },
+  400: {
+    slidesPerView: 2,
+  },
+  768: {
+    slidesPerView: 3,
+  },
+  1020: {
+    slidesPerView: 4,
+  },
+},
+});
